0836680262c88cee681d9e2de079ee72fb46dc82
Taylor Hemby-Ginsberg <98064323+ThembyG@users.noreply.github.com>
2024-04-27 17:07:11 -0400

    Fixed type issues and generate both images

--- test.py
+++ test.py
@@ -44,8 +44,11 @@ all_errors = [[], [], [], [], []]
 # epsilon_vals = [1.4]
 num_queries = len(noise_queries)
 all_non = []
-all_times = []
-
+pre_times = []
+fix_all = []
+fix_one = []
+debug_all =[]
+debug_one = []
 # for i in range(5):
 #         start_nonnoise = time.time()
 #         non_temp = pd.DataFrame(data = sqldf(non_noise_queries[i]))
@@ -81,6 +84,8 @@ all_times = []
 #     #all_times.append(noise_times)
 #     #all_errors.append(error_rate)
 
+
+#Pre fix
 for i in range(num_queries):
     reader = snsql.from_df(df_noisy, privacy=privacy, metadata=meta_path)
     start_noise = time.time()
@@ -89,21 +94,60 @@ for i in range(num_queries):
     # print(f'temp b4 clip is {temp}')
     end_noise = time.time()
     total_time = end_noise-start_noise
-    wait_time = np.random.laplace(loc=total_time, scale=1, size=1)
+    #wait_time = np.random.laplace(loc=total_time, scale=1, size=1)
     temp = temp.drop([0])
     temp = temp.apply(pd.to_numeric)
     noise_dfs.append(temp)
-    
-    all_times.append(end_noise - start_noise)
-    time.sleep(float(abs(wait_time-total_time)))
+    pre_times.append(total_time)
+    # all_times.append(abs(wait_time[0]))
+    # time.sleep(float(abs(wait_time-total_time)))
     
     # max_noise = float(temp.max().max())
     # max_non   = float(all_non[i].max().max())
     #print(f"noise max is: {max_noise} , non_max is: {max_non}")
     # all_errors[i].append(abs(((max_noise)-(max_non)) / (max_non)))
 
-    
-title = 'Time Differences Between All and Pickup 1'
+#Post Fix
+for k in range(10):
+    #temp_times = []
+    for i in range(num_queries):
+        print(f'i is: {i}')
+        reader = snsql.from_df(df_noisy, privacy=privacy, metadata=meta_path)
+        start_noise = time.time()
+        #print(noise_queries[i])
+        temp = pd.DataFrame(data=reader.execute(noise_queries[i]))
+        # print(f'temp b4 clip is {temp}')
+        end_noise = time.time()
+        total_time = end_noise-start_noise
+        wait_time = np.random.laplace(loc=0, scale = 0.01, size=1)
+        sleep_time = abs(wait_time[0])
+        #sleep_time = float(abs(abs(wait_time[0]) - total_time))
+        
+        #randi = random.randint(1, 5)
+        #if sleep_time > (total_time * 5):
+        #    sleep_time = total_time * randi
+        #print(f'Sleep time is: {sleep_time}')
+        #print(f'Total time is: {total_time}')
+        # all_times.append(abs(wait_time[0]))
+        time.sleep(sleep_time)
+        final_noise = time.time()
+        final_time = final_noise - start_noise
+        #print(f'b4 time is: {total_time}')
+        #print(f'Final time is: {final_time}')
+
+        if i == 0: 
+            fix_all.append(final_time)
+            debug_all.append(('i is ' + str(i)))
+        else:
+            fix_one.append(final_time)
+            debug_one.append(('i is ' +  str(i)))
+        temp = temp.drop([0])
+        temp = temp.apply(pd.to_numeric)
+        noise_dfs.append(temp)
+    #fix_times.append(temp_times)
+
+title = ['Time Differences Between All and Pickup 1 Without Noise', 
+        'Time Differences Between All and Pickup 1 With Noise' ]
         #   Top Pickup Times Difference',
         #   'Pickup Times Error',
         #   'Top Dropoff Times Difference',
@@ -123,14 +167,44 @@ fig, ax = plt.subplots(figsize = (8,8))
 #
 #print((noise_dfs[j]))
 #print(curr_list)
-ax.bar(string_vals, all_times, width= .8)
+ax.bar(string_vals, pre_times, width= .8)
 ax.xaxis.set_tick_params(pad = 5)
 ax.yaxis.set_tick_params(pad = 10)
 ax.autoscale(enable=True)
-ax.set_title(title,loc ='center' )
+ax.set_title(title[0],loc ='center' )
+
+plt.savefig('diff_before_fix.png')
+
+# fig, ax = plt.subplots(figsize = (8,8))
+# #
+# #print((noise_dfs[j]))
+# #print(curr_list)
+# ax.bar(string_vals, fix_times, width= .8)
+# ax.xaxis.set_tick_params(pad = 5)
+# ax.yaxis.set_tick_params(pad = 10)
+# ax.set_title(title[1],loc ='center' )
+
+# plt.savefig('diff_after_fix.png')
+
+print(debug_all)
+print(debug_one)
+
+index = np.arange(10)
+bar_width = 0.35
+
+fig, ax = plt.subplots()
+all = ax.bar(index, fix_one, bar_width, label="All", color = '#3E8EDE')
+
+zone_1 = ax.bar(index+bar_width, fix_all, bar_width, label="Zone 1", color = '#512C1D')
+
+ax.set_xlabel('Query')
+ax.set_ylabel('Time in Seconds')
+ax.set_title('Time Differences Between All and Pickup 1 With Noise')
+ax.set_xticks(index + bar_width / 2)
+ax.set_xticklabels(['Trial ' + str(x + 1) for x in range (10)])
+ax.legend()
 
 plt.savefig('diff_after_fix.png')
-
 # loops = len(titles)
 # string_vals = [str(x) for x in epsilon_vals]
 # for j in range(loops):
